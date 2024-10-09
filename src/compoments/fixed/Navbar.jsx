ee64b7db67a1f56fcd6a0908057f6b4a68f4f38b
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-09 18:03:15 +0600

    ck

--- src/compoments/fixed/Navbar.jsx
+++ src/compoments/fixed/Navbar.jsx
@@ -8,8 +8,6 @@ import { ThemeContext } from "../mode/DarkAndLight";
 
 const Navbar = () => {
   const { theme, taggleTheme } = useContext(ThemeContext);
-  const { theme, taggleTheme } = useContext(ThemeContext);
-  const { theme, taggleTheme } = useContext(ThemeContext);
   return (
     <nav className="">
       <div className="py-8 dark:bg-gray-800">
