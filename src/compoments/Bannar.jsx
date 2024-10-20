63095639781ba82bc2df23bb1c40d89285f76048
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-20 19:27:45 +0600

    banner text

--- src/compoments/Bannar.jsx
+++ src/compoments/Bannar.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const Bannar = () => {
   return (
-    <section>
+    <section className="dark:bg-gray-800 dark:text-white">
       <div className="container">
         <div>
-          
+          <h2 className="font-primary font-semibold text-6xl leading-[76%] w-[657px]">Lessons and insights from 8 years</h2>
+          <p className="">
+            Where to grow your business as a photographer: site or social media?
+          </p>
         </div>
       </div>
     </section>
