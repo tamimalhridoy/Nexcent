9b632e07ef22e7abb1e82871496e640824423dc4
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-27 23:18:25 +0600

    banner

--- src/compoments/Bannar.jsx
+++ src/compoments/Bannar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
 
 const Bannar = () => {
   return (
@@ -6,16 +7,18 @@ const Bannar = () => {
       <div className="container">
         <div className="flex py-24">
           <div className=" my-16">
-            <h2 className="font-primary font-semibold text-6xl w-[657px]">
-              Lessons and insights from 8 years
-            </h2>
-            <p className="mt-4 mb-8 font-primary font-normal text-base leading-6">
-              Where to grow your business as a photographer: site or social
-              media?
-            </p>
-            <button className="font-medium text-sm font-primary py-3 px-9 bg-brand rounded-lg">
-              Register
-            </button>
+            <div data-aos="fade-right">
+              <h2 className="font-primary font-semibold text-6xl w-[657px]">
+                Lessons and insights from 8 years
+              </h2>
+              <p className="mt-4 mb-8 font-primary font-normal text-base leading-6">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="font-medium text-sm font-primary py-3 px-9 bg-brand rounded-lg">
+                Register
+              </button>
+            </div>
           </div>
           <img
             src="/banner-images01.png"
